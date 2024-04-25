ff61701d941f8e84164d4fa8ca87add494127d5d
Tushar Sharma <tttushar.789@gmail.com>
2024-04-25 13:24:34 +0530

    fourth commit

--- src/app/[meetupId]/page.js
+++ src/app/[meetupId]/page.js
@@ -1,6 +1,13 @@
 import MeetupDetail from "../components/meetups/MeetupDetail";
-
-function MeetupDetails() {
+import { MongoClient } from "mongodb";
+async function MeetupDetails() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://tushar:tushar@1@cluster0.dcudhdc.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0"
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+  client.close();
   return (
     <MeetupDetail
       image="https://assets.weforum.org/article/image/responsive_large_webp_v2gQQH6Qq5EAJSemo1Yqie27aYBOHE4pSwA6jSkFHKg.webp"
